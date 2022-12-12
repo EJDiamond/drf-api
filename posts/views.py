87b38b29bf91fb88e59b7f96925ef1d2b11050bc
EJDiamond <emilydiamond321@gmail.com>
2022-12-12 21:26:52 +0000

    Add postlist view, defined get method to list all posts and the post method to create a user post

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostList(APIView):
+    serializer_class = PostSerializer
+    # ensures only the user instances can create posts
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
+    def get(self, request):
+        # retrieve all post instances from the database
+        posts = Post.objects.all()
+        # serialize them and pass many=true with the request in the context object
+        serializer = PostSerializer(
+            posts, many=True, context={'request': request}
+        )
+        # return serialized data in the response
+        return Response(serializer.data)
+
+    def post(self, request):
+        # deserialize post data passing in whatever the user sends in the request itself in the context object
+        serializer = PostSerializer(
+            data=request.data, context={'request': request}
+        )
+        # if data is valid save the post and associate it with the current user
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
