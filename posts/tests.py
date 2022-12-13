b28fe70d0a95434355f5f1c785cacb54698f368f
EJDiamond <emilydiamond321@gmail.com>
2022-12-13 22:32:00 +0000

    Add post detail tests

--- posts/tests.py
+++ posts/tests.py
@@ -46,7 +46,46 @@ class PostListViewTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
+class PostDetailViewTests(APITestCase):
+    def setUp(self):
+        # create two users to test
+        adam = User.objects.create_user(username='adam', password='pass')
+        brian = User.objects.create_user(username='brian', password='pass')
+        # create two posts, one for each user
+        Post.objects.create(
+            owner=adam, title='a title', content='adams content'
+        )
+        Post.objects.create(
+            owner=brian, title='another title', content='brians content'
+        )
 
+    # test to see if it is possible to retrieve a post with a valid id
+    def test_can_retrieve_post_using_valid_id(self):
+        # create a new test method and make  a get request to fetch the first post
+        response = self.client.get('/posts/1/')
+        # assert the fetched post’s title is  appropriate, and set the status to 200
+        self.assertEqual(response.data['title'], 'a title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_can_not_retrieve_post_using_invalid_id(self):
+        response = self.client.get('/posts/333/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
+    def test_user_can_update_own_test(self):
+        # log in as 'adam'
+        self.client.login(username='adam', password='pass')
+        # send a put request to  a url with their post’s ID
+        response = self.client.put(('/posts/1/'), {'title': 'a new title'})
+        # we’ll fetch that post  from the database by ID
+        post = Post.objects.filter(pk=1).first()
+        # Once we have it saved to a variable, we’ll  test that the change to the post title has
+        # been persisted
+        self.assertEqual(post.title, 'a new title')
+        # We’ll also make sure the  200 OK code is sent with the response
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cant_update_post_they_dont_own(self):
+        self.client.login(username='brian', password='pass')
+        response = self.client.put(('/posts/1/'), {'title': 'a new title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
