6d6f71d846d20775cbde7f2802e3384fe4c6ef87
EJDiamond <emilydiamond321@gmail.com>
2022-12-28 22:35:26 +0000

    Add allowed host

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -67,9 +67,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
-    'https://drf-api-ed.herokuapp.com/',
-    'localhost',
-    ]
+   os.environ.get('ALLOWED_HOST'),
+   'localhost',
+]
 
 
 # Application definition
